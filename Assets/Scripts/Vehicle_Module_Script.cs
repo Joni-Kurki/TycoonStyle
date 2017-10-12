000de0d9739b9b52bfb2887d45252aa29b4cc6e1
Munkittaja <joni.kurki@gmail.com>
2017-10-12 21:29:58 +0300

    added some space vehicle and modules. early protos

---
+++ Assets/Scripts/Vehicle_Module_Script.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle_Module {
+
+    public string _name { get; set; }
+    public int _moduleSize { get; set; }
+    public Vehicle_Module_List _moduleList;
+
+    public enum _vehicleModuleType {
+        Storage = 1,
+        Weapon,
+        Tool
+    }
+}
+
+public class Storage_Vehicle_Module : Vehicle_Module {
+
+    Vehicle_Module._vehicleModuleType _moduleType;
+
+    public Storage_Vehicle_Module(string name, int size) {
+        _name = name;
+        _moduleType = _vehicleModuleType.Storage;
+        _moduleSize = size;
+        _moduleList = new Vehicle_Module_List();
+    }
+}
+
+public class Tool_Vehicle_Module : Vehicle_Module {
+
+    Vehicle_Module._vehicleModuleType _moduleType;
+
+    public Tool_Vehicle_Module(string name, int size) {
+        _name = name;
+        _moduleType = _vehicleModuleType.Tool;
+        _moduleSize = size;
+    }
+}
+
+public class Vehicle_Module_List {
+
+    public List<Object> _moduleList;
+
+    public Vehicle_Module_List() {
+        _moduleList = new List<Object>();
+    }
+
+    public List<Object> GetModules() {
+        return _moduleList;
+    }
+    public void AddModule(Object module) {
+        _moduleList.Add(module);
+    }
+}
---
+++ Assets/Scripts/Vehicle_Script.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle {
+        
+    public string _name { get; set; }
+    public bool _canFly { get; set; }
+    // moduulien lukumäärät
+    public int _numberOfStorageModules { get; set; }
+    public int _numberOfWeaponModules { get; set; }
+    public int _numberOfToolModules { get; set; }
+
+    public enum _vehicleType {
+        Production = 1,
+        War,
+        Gatherer
+    }
+
+    public Vehicle() {
+
+    }
+}
+
+public class SpaceMiner_Vehicle : Vehicle {
+
+    public SpaceMiner_Vehicle(int numberOfStorageModules, int numberOfWeaponModules, int numberOfToolModules) {
+        _canFly = true;
+        _numberOfStorageModules = numberOfStorageModules;
+        _numberOfWeaponModules = numberOfWeaponModules;
+        _numberOfToolModules = numberOfToolModules;
+    }
+
+}
+
+public class War_Vehicle : Vehicle {
+
+    public War_Vehicle() {
+
+    }
+}
+
