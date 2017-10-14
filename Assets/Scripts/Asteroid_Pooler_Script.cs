86b7fd7d7ce0b9122ce170e4d5d0cf9a7e0d7c40
Munkittaja <joni.kurki@gmail.com>
2017-10-14 12:53:08 +0300

    Made the asteroid spawning script more versatible. We can now control many settings from editor. Also the asteroid spawning work in a donut shape, where we can define the offsets.

--- Assets/Scripts/Asteroid_Pooler_Script.cs
+++ Assets/Scripts/Asteroid_Pooler_Script.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class Asteroid_Pooler_Script : MonoBehaviour {
 
     public GameObject _asteroidPrefab;
-    public int _asteroidNumber;
-    const int MAX_ASTEROID_NUMBER = 100;
-    public Vector3 _spanwLocation;
 
-	// Use this for initialization
-	void Start () {
-        InstantiateAsteroids(_spanwLocation, -2,2);
+    public Vector3 _spanwLocation;
+    public float _distanceOffsetMin;
+    public float _distanceOffsetMax;
+    public float _yOffsetMin;
+    public float _yOffsetMax;
+    public int _numberOfAsteroidsMin;
+    public int _numberOfAsteroidsMax;
+
+    // Use this for initialization
+    void Start () {
+        InstantiateAsteroids(_spanwLocation);
     }
 	
 	// Update is called once per frame
@@ -19,11 +24,27 @@ public class Asteroid_Pooler_Script : MonoBehaviour {
 		
 	}
 
-    void InstantiateAsteroids(Vector3 location, float randomMin, float randomMax) {
-        int randomN = Random.Range(0, MAX_ASTEROID_NUMBER);
+    /// <summary>
+    /// Instantiates n amount of asteroids in a donut shaped system. 
+    /// </summary>
+    /// <param name="location"></param>
+    /// <param name="randomMin"></param>
+    /// <param name="randomMax"></param>
+    void InstantiateAsteroids(Vector3 location) {
+
+        int randomN = Random.Range(_numberOfAsteroidsMin, _numberOfAsteroidsMax);
         for (int i = 0; i < randomN; i++) {
-            Vector3 offset = new Vector3(Random.Range(randomMin, randomMax), Random.Range(randomMin / Mathf.PI, randomMax / Mathf.PI), Random.Range(randomMin, randomMax));
-            Instantiate(_asteroidPrefab, _spanwLocation + offset, Quaternion.identity, this.transform);
+            // Let's random the angle
+            float angle = Random.Range(0f, 360f);
+            // Random the distances and y offset
+            float randomDistance = Random.Range(_distanceOffsetMin, _distanceOffsetMax);
+            float randomYOffset = Random.Range(_yOffsetMin, _yOffsetMax);
+            // Instantiate @ desired location
+            var obj = Instantiate(_asteroidPrefab, _spanwLocation, Quaternion.identity, this.transform);
+            // Now let's rotate the object and move it to desired angle with randomized distance
+            obj.transform.Rotate(new Vector3(0, angle, 0));
+            obj.transform.Translate(new Vector3(randomDistance, randomYOffset, randomDistance));
+            //Debug.DrawLine(Vector3.zero, obj.transform.position, Color.red, 30);
         }
     }
 }
---
+++ Assets/Scripts/Asteroid_Rotator_Script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid_Rotator_Script : MonoBehaviour {
+
+    float _rotationSpeed;
+    bool _isInitiated;
+    Rigidbody _rb;
+
+    // Use this for initialization
+    void Start () {
+        _rb = GetComponent<Rigidbody>();
+        _isInitiated = false;
+        _rotationSpeed = Random.Range(0.01f, 3f);
+    }
+
+    private void FixedUpdate() {
+        if (!_isInitiated) {
+            _rb.angularVelocity = Random.insideUnitSphere * _rotationSpeed;
+            _isInitiated = true;
+        }
+    }
+}
