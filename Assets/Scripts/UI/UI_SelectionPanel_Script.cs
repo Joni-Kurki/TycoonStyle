e8644d084df006f4ea13f17cd94960e87b40d63f
Munkittaja <joni.kurki@gmail.com>
2017-10-19 19:52:16 +0300

    Started making planet selection UI a bit

---
+++ Assets/Scripts/UI/UI_SelectionPanel_Script.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_SelectionPanel_Script : MonoBehaviour {
+
+    // Grabs all the planets we want to show at UI
+    public GameObject [] _planets;
+    public GameObject _planetChildPrefab;
+    public Sprite [] _planetsSprite;
+
+	// Use this for initialization
+	void Start () {
+        _planets = GameObject.FindGameObjectsWithTag("Planet_Linkable");
+        SetPlanetsToUI();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SetPlanetsToUI() {
+
+        for (int i = 0; i < _planets.Length; i++) {
+            var go = Instantiate(_planetChildPrefab, transform);
+            Image img = go.GetComponent<Image>();
+            img.sprite = _planetsSprite[0];
+        }
+    }
+
+    public void TestClick() {
+
+    }
+
+    private void OnMouseDown() {
+        Debug.Log("Click" + this.name);
+    }
+}
